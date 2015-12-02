53cbc89d220463e7ea9c1278861bdf64c0bb2b8d
Stefan van der Haven <stefan@steeffie.net>
2015-12-02 15:04:10 +0100

    Dev and postinstall fixes

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,13 +3,18 @@ var path = require('path');
 var util = require('util');
 var TEST_DIR = path.resolve(__dirname, '..', 'test');
 var SRC_DIR = path.resolve(__dirname, '..', 'fuse');
+
 var child_process = require('child_process');
 
 function runIt() {
   console.log('Started at ' + Date().toString());
-  var command = "rm -rf dist coverage && node_modules/.bin/tsc && node_modules/.bin/istanbul cover --report text --report html --print detail -i 'dist/**/*.js' --include-all-sources node_modules/.bin/_mocha -- dist/test/index.js";
+  var commands = [
+    "rm -rf build coverage",
+    "node_modules/.bin/tsc --project " + __dirname,
+    "node_modules/.bin/istanbul cover --report text --report html --print detail -i 'build/**/*.js' --include-all-sources node_modules/.bin/_mocha -- build/test/index.js"
+  ];
   try {
-    child_process.execSync(command, { stdio: [0, 1, 2]});
+    child_process.execSync(commands.join(" && "), { stdio: [0, 1, 2]});
   } catch(e) {
 
   }
